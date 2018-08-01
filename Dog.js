c39640d134d4a3fe00a1cb36dd4584bff20be660
hifon168 <bill800208@163.com>
2018-08-01 17:33:50 +0800

    Tom又咬了Tom

--- Dog.js
+++ Dog.js
@@ -7,8 +7,13 @@ function Dog(name) {
 var tom=new Dog('Tom')
 var jerry=new Dog('Jerry')
 tom.bite(jerry)
+jerry.bite.call(tom,tom)
+tom.bite.call(tom,tom)
+
 
 // 你先写个程序吧，里面定义一个叫做 Dog 的类，构造函数里传入狗的名字，
 // 成员函数有个bite，参数为别的狗，调用会打印“xx咬了xx”。
 // 然后 new Dog("Tom"), new Dog("Jerry")，再让Tom咬Jerry。
 // 文件写完放到github上，把地址发给我，我再给下一步指示。
+
+// 调用 jerry.bite 打印 Tom咬了Tom
